21bedb0aa0bf2a1489c4daddb59ff8feca339485
DataOmbudsman <data.ombudsman@tutanota.com>
2019-12-13 20:11:43 +0100

    Refactor: move updater out of incdbscan core
    Signed-off-by: DataOmbudsman <data.ombudsman@inbox.lv>

--- src/incrementaldbscan/incrementaldbscan.py
+++ src/incrementaldbscan/incrementaldbscan.py
@@ -1,10 +1,9 @@
 import warnings
 from typing import Dict, Iterable
 
-import networkx as nx
-
 from src.incrementaldbscan._labels import ClusterId, _Labels
 from src.incrementaldbscan._objects import _Object, _Objects, ObjectId
+from src.incrementaldbscan._updater import _Updater
 from src.dbscan._dbscanbase import _DBSCANBase
 
 
@@ -18,6 +17,8 @@ class IncrementalDBSCAN(_DBSCANBase):
         super().__init__(eps, min_pts)
         self._labels = _Labels()
         self._objects = _Objects()
+        self._updater = _Updater(self)
+
         self.labels: Dict[ObjectId, ClusterId] = self._labels.get_all_labels()
 
     def add_object(self, object_value, object_id: ObjectId):
@@ -28,7 +29,7 @@ class IncrementalDBSCAN(_DBSCANBase):
 
         if not self._labels.has_label(object_id):
             object_to_insert = _Object(object_value, object_id)
-            self._insertion(object_to_insert)
+            self._updater.insertion(object_to_insert)
 
         else:
             warnings.warn(
@@ -54,7 +55,7 @@ class IncrementalDBSCAN(_DBSCANBase):
         """Remove object, given by its ID, from clustering."""
 
         if self._labels.has_label(object_id):
-            self._deletion(object_id)
+            self._updater.deletion(object_id)
 
         else:
             warnings.warn(
@@ -69,155 +70,6 @@ class IncrementalDBSCAN(_DBSCANBase):
 
         for object_id in object_ids:
             self.remove_object(object_id)
-
-    def _insertion(self, object_to_insert: _Object):
-        # print('\nInserting', object_to_insert.id)  # TODO
-        self._objects.add_object(object_to_insert)
-        self._labels.set_label(
-            object_to_insert, self.CLUSTER_LABEL_UNCLASSIFIED)
-
-        neighbors = self._objects.get_neighbors(object_to_insert, self.eps)
-        self._update_neighbor_counts_after_insert(object_to_insert, neighbors)
-
-        new_core_neighbors, old_core_neighbors = \
-            self._filter_core_objects_split_by_novelty(neighbors)
-
-        if not new_core_neighbors:
-            print('\nnot new_core_neighbors')  # TODO
-            # If there is no new core object,
-            # only the new object has to be put in a cluster.
-
-            if old_core_neighbors:
-                print('old_core_neighbors')
-                # If there are already core objects near to the new object,
-                # the new object is put in the most recent cluster. This is
-                # alike to case "Absorption" in the paper but not defined
-                # there.
-                label_of_new_object = max([
-                    self._labels.get_label(obj) for obj in old_core_neighbors
-                ])
-
-            else:
-                print('not old_core_neighbors')
-                # If the new object does not have any core neighbors,
-                # it becomes a noise. Called case "Noise" in the paper.
-                label_of_new_object = self.CLUSTER_LABEL_NOISE
-
-            self._labels.set_label(
-                    object_to_insert, label_of_new_object)
-            return
-
-        print('\nnew_core_neighbors')  # TODO
-        neighbors_of_new_core_neighbors = \
-            self._get_neighbors_of_objects(new_core_neighbors)
-
-        update_seeds = self._get_update_seeds(neighbors_of_new_core_neighbors)
-
-        connected_components_in_update_seeds = \
-            self._get_connected_components_in_update_seeds(
-                update_seeds, neighbors_of_new_core_neighbors)
-
-        for component in connected_components_in_update_seeds:
-            max_cluster_label = max([
-                self._labels.get_label(obj) for obj in component
-            ])
-
-            if max_cluster_label < 0:
-                print('max_cluster_label < 0')  # TODO
-                # If in a connected component of update seeds there are only
-                # unclassified and noise objects, a new cluster is created.
-                # Corresponds to case "Creation" in the paper.
-                for obj in component:
-                    self._labels.set_label(obj, self._next_cluster_label)
-                self._next_cluster_label += 1
-
-            else:
-                print('max_cluster_label >= 0')  # TODO
-                # If in a connected component of updates seeds there are
-                # already clustered objects, all objects in the component
-                # will be merged into the most recent cluster.
-                # Corresponds to cases "Absorption" and "Merge" in the paper.
-                for obj in component:
-                    self._labels.set_label(obj, max_cluster_label)
-
-                # TODO környezetet is ???
-                # Mindet update-eljük vagy mergeöket számon tartjuk
-
-        # Finally all neighbors of each new core object inherits a label from
-        # its new core neighbor, thereby covering border and noise objects.
-        self._set_cluster_label_to_that_of_new_core_neighbor(
-            neighbors_of_new_core_neighbors
-        )
-
-    def _update_neighbor_counts_after_insert(self, new_object, neighbors):
-        for neighbor in neighbors:
-            neighbor.neighbor_count += 1
-        new_object.neighbor_count = len(neighbors)
-
-    def _filter_core_objects_split_by_novelty(self, objects):
-        new_cores = set()
-        old_cores = set()
-
-        for obj in objects:
-            if obj.neighbor_count == self.min_pts:
-                new_cores.add(obj)
-            elif obj.neighbor_count > self.min_pts:
-                old_cores.add(obj)
-
-        return new_cores, old_cores
-
-    def _get_neighbors_of_objects(self, objects):
-        neighbors = dict()
-
-        for obj in objects:
-            neighbors[obj] = \
-                self._objects.get_neighbors(obj, self.eps)
-
-        return neighbors
-
-    def _get_update_seeds(self, neighbors_of_new_core_neighbors):
-        seeds = set()
-
-        for neighbors in neighbors_of_new_core_neighbors.values():
-            core_neighbors = [obj for obj in neighbors
-                              if obj.neighbor_count >= self.min_pts]
-            seeds.update(core_neighbors)
-
-        return seeds
-
-    def _get_connected_components_in_update_seeds(
-            self,
-            update_seeds: Iterable,
-            stored_neighbors: Dict):
-
-        G = nx.Graph()
-
-        for seed in update_seeds:
-
-            if seed in stored_neighbors:
-                neighbors = stored_neighbors[seed]
-            else:
-                neighbors = \
-                    self._objects.get_neighbors(seed, self.eps)
-
-            for neighbor in neighbors:
-                if neighbor.neighbor_count >= self.min_pts:
-                    G.add_edge(seed, neighbor)
-
-        return nx.connected_components(G)
-
-    def _set_cluster_label_to_that_of_new_core_neighbor(
-            self,
-            neighbors_of_new_core_neighbors):
-
-        for new_core, neighbors in neighbors_of_new_core_neighbors.items():
-            label = self._labels.get_label(new_core)
-            for neighbor in neighbors:
-                self._labels.set_label(neighbor, label)
-
-    def _deletion(self, object_id):
-        # Do lot of stuff, then:
-        self._labels.delete_label(object_id)
 
 
 class IncrementalDBSCANWarning(Warning):
